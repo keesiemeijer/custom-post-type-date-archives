b9cc9af577ba1b251738334787319b4a9b1d4170
keesiemeijer <keesie.meijer@gmail.com>
2016-02-20 16:00:51 +0100

    Add uninstall tests

--- uninstall.php
+++ uninstall.php
@@ -14,3 +14,17 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit;
 
 global $wp_rewrite;
 $wp_rewrite->flush_rules();
+
+if ( is_multisite() ) {
+	global $wpdb;
+	$blogs = $wpdb->get_results( "SELECT blog_id FROM {$wpdb->blogs}", ARRAY_A );
+	if ( $blogs ) {
+		foreach ( (array) $blogs as $blog ) {
+			switch_to_blog( $blog['blog_id'] );
+			delete_option( 'custom_post_type_date_archives' );
+		}
+		restore_current_blog();
+	}
+} else {
+	delete_option( 'custom_post_type_date_archives' );
+}
