e5f60840f57115c8155d2ea6d5ba6be69b972f0b
Kees Meijer <keesie.meijer@gmail.com>
2019-12-06 13:48:44 +0100

    Use correct deprecated DocBlock tag

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -13,7 +13,7 @@
  * Returns public post types that have archives and are displayed in the admin menu.
  *
  * @since 2.1.0
- * @since 2.5.0 Deprecated.
+ * @deprecated 2.5.0 Use cptda_get_post_types().
  *
  * @param string $type Return type 'names' or 'objects'.
  * @return array Array with post types.
@@ -31,12 +31,11 @@ function cptda_get_admin_post_types( $type = 'names' ) {
  * Get the current date archive custom post type name.
  *
  * @since 1.0
- * @since 2.5.0 Deprecated.
+ * @deprecated 2.5.0 Use cptda_get_queried_date_archive_post_type().
  *
  * @return string Post type name if used in a custom post type date archive. Else empty string.
  */
 function cptda_get_date_archive_cpt() {
-
 	_deprecated_function( __FUNCTION__, '2.5.0', 'cptda_get_queried_date_archive_post_type' );
 
 	return cptda_get_queried_date_archive_post_type();
